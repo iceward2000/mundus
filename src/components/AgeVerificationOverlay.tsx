32456952ba4c7b4b5d355289cdec440e879c9a6c
iceward2000 <huseyin.gemici2000@gmail.com>
2026-04-15 21:17:11 +0300

    percentage on the evet button removed

--- src/components/AgeVerificationOverlay.tsx
+++ src/components/AgeVerificationOverlay.tsx
@@ -501,18 +501,6 @@ export default function AgeVerificationOverlay() {
                     >
                       <StableLocaleText tKey="overlay.yes" nowrap className="text-inherit" />
                     </span>
-                    {loadProgress > 0 && (
-                      <span
-                        className={clsx(
-                          "text-[10px] tabular-nums font-light transition-colors duration-500",
-                          loadComplete
-                            ? "text-white/55 group-hover:text-black/70"
-                            : "text-white/40"
-                        )}
-                      >
-                        {loadProgress}%
-                      </span>
-                    )}
                   </div>
                 </button>
 
